dbeedef03c6209db276ab7aa4ce2fc61bac507d3
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2019-08-16 20:12:12 +0530

    CLEAR DATABASE, CREATE COMMENTS MODEL

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
