d10e03026d30ed4ef9a42b9b94153d28d904d235
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2019-08-17 13:04:38 +0530

    CREATE COMMENT FORM IN POST DETAILS
    Create a working comment form in each post details view, add css styling
    to comment form

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
+from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
 
 
 class PostListView(ListView):
@@ -24,8 +25,37 @@ class UserPostListView(ListView):
         return Post.objects.filter(author=user).order_by("-date")
 
 
-class PostDetailView(DetailView):
+# class PostDetailView(DetailView):
+#     model = Post
+
+
+class PostView(DetailView):
     model = Post
+    pass
+
+
+class CommentView(CreateView):
+    model = Comment
+    fields = ["content"]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.post = self.get_object(Post.objects.all())
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.get_object(Post.objects.all()).pk})
+    pass
+
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = PostView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs) :
+        view = CommentView.as_view()
+        return view(request, *args, **kwargs)
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -63,6 +93,15 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         else:
             return False
+
+
+# class CommentCreateView(LoginRequiredMixin, CreateView):
+#     model = Comment
+#     fields = ["content"]
+#
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         return super().form_valid(form)
 
 
 def about(request):
