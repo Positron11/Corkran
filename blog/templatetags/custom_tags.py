418f8358bd74f2917d35ba08ca11124a198cf785
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2019-08-28 20:33:47 +0530

    FIX NEW-POSTS, REPOSITION PAGINATION
    Create new templatetags for new posts, create separate html file for 
    paginator, change position of pagination to top and bottom of posts, 
    restyle pagination

--- blog/templatetags/custom_tags.py
+++ blog/templatetags/custom_tags.py
@@ -1,7 +1,22 @@
-from blog.models import Announcement
+from blog.models import Announcement, Post
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
 from django import template
 
 register = template.Library()
+
+
+@register.simple_tag
+def latest_post():
+    post = Post.objects.last()
+    return mark_safe(f"<a href='{reverse_lazy('post-detail', kwargs={'pk': post.pk})}'>{post.title}</a>")
+
+
+@register.simple_tag
+def user_latest_post(username):
+    post = User.objects.filter(username=username).first().post_set.last()
+    return mark_safe(f"<a href='{reverse_lazy('post-detail', kwargs={'pk': post.pk})}'>{post.title}</a>")
 
 
 @register.simple_tag
