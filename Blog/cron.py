673680c8d28c966c4893743990650b8dda834e97
Aarush Kumbhakern <aarush@nefarious.nefarious.org>
2020-06-02 00:35:38 +0530

    SEND NOTIFICATIONS OF NEW REPLIES TO COMMENTS

--- Blog/cron.py
+++ Blog/cron.py
@@ -8,9 +8,11 @@ from Blog.models import Comment
 
 class NewCommentsEmailNotification(CronJobBase):
 	# run daily
-	schedule = Schedule(run_every_mins=1440)
+	schedule = Schedule(run_at_times=['24:00'])
 	code = 'Corkran.new_comments_email_notification'
 
+
+	# main cronjob
 	def do(self):
 		# get queryset of day-old comments
 		time_threshold = datetime.now() - timedelta(hours=24)
@@ -18,42 +20,74 @@ class NewCommentsEmailNotification(CronJobBase):
 
 		# if any new comments
 		if (len(new_comments) > 0):
-			# new empty dataset
-			data = dict()
+			self.new_comments_on_article(new_comments)
+			self.new_replies(new_comments)
 
-			# create a dictionary of recipients to their articles
-			for comment in new_comments:
-				data[comment.article.author] = dict()
+	
+	# notifications to article authors about new comments
+	def new_comments_on_article(self, new_comments):
+		# new empty dataset
+		data = dict()
 
-			# create an empty list for comments for each article
-			for comment in new_comments:
-				data[comment.article.author][comment.article] = list()
+		# create a dictionary of recipients to their articles
+		for comment in new_comments:
+			data[comment.article.author] = dict()
 
-			# add comments into appropriate lists
-			for comment in new_comments:
-				data[comment.article.author][comment.article].append(comment)
+		# create an empty list for comments for each article
+		for comment in new_comments:
+			data[comment.article.author][comment.article] = list()
 
-			# standard email values
-			from_email = 'Corkran <noreply@corkran.pythonanywhere.com>' 
-			subject = 'New comments on your Corkran articles' 
-			
-			# for each recipient...
-			for recipient in data:
-				context = {
-					"recipient": recipient.username, 
-					"articles": data[recipient], 
-					"protocol": "https", 
-					"domain": "corkran.pythonanywhere.com"
-				}
-
-				# html message and plaintext fallback
-				html_message = render_to_string('Blog/new_comments_email.html', context)
-				plain_message = render_to_string('Blog/new_comments_email.txt', context)
-
-				# send email
-				send_mail(from_email=from_email, recipient_list=[recipient.email], subject=subject, message=plain_message, html_message=html_message)
+		# add comments into appropriate lists
+		for comment in new_comments:
+			data[comment.article.author][comment.article].append(comment)
+		
+		self.send_email(data, "Blog/new_comments_email.html", "Blog/new_comments_email.txt")
 
 
+	# notifications to comment authors about replies
+	def new_replies(self, new_comments):
+		# get replies out of new comments
+		new_replies = new_comments.filter(parent__in=Comment.objects.all())
+
+		# new empty dataset
+		data = dict()
+
+		# create a dictionary entry for each recipient
+		for comment in new_replies:
+			data[comment.parent.author] = dict()
+
+		# create an empty list for the replies of each parent comment
+		for comment in new_replies:
+			data[comment.parent.author][comment.parent] = list()
+
+		# place all replies in their appropriate entries
+		for comment in new_replies:
+			data[comment.parent.author][comment.parent].append(comment)
+
+		self.send_email(data, "Blog/new_replies_email.html", "Blog/new_replies_email.txt")
+
+	
+	# email function
+	def send_email(self, data, html_template, text_template):
+		# standard email values
+		from_email = 'Corkran <noreply@corkran.pythonanywhere.com>' 
+		subject = 'New comments on your Corkran articles' 
+		
+		# for each recipient...
+		for recipient in data:
+			context = {
+				"recipient": recipient.username, 
+				"data": data[recipient], 
+				"protocol": "https", 
+				"domain": "corkran.pythonanywhere.com"
+			}
+
+			# html message and plaintext fallback
+			html_message = render_to_string(html_template, context)
+			plain_message = render_to_string(text_template, context)
+
+			# send email
+			send_mail(from_email=from_email, recipient_list=[recipient.email], subject=subject, message=plain_message, html_message=html_message)
 
 
 
